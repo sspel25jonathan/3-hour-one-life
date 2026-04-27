28f983c86fd39ffe0754b49e55cd49d171aea9c4
sspel25jonathan <sspel25jope@axevalla.fhsk.se>
2026-04-27 16:05:28 +0200

    wow

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -1,21 +1,21 @@
-using UnityEditor;
 using UnityEngine;
+using System.Collections;
+using UnityEditor;
 
-[CustomEditor(typeof(MapGenerator))]
-public class MapGeneratorEditor : Editor
-{
-public override void OnInspectorGUI()
-    {
-        MapGenerator mapGen = (MapGenerator)target;
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
 
-        DrawDefaultInspector();
+	public override void OnInspectorGUI() {
+		MapGenerator mapGen = (MapGenerator)target;
 
-        if (GUILayout.Button("Generate"))
-        {
-       
-            mapGen.GenerateMap();
-        }
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
 
-        
-    }
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor ();
+		}
+	}
 }
--- Assets/Scripts/Map Generator/Editor.cs
+++ Assets/Scripts/Map Generator/Editor.cs
@@ -1,22 +1,21 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
-[CustomEditor(typeof(MapGenerator))]
-public class MapGeneratorEdit : Editor
-{
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor {
 
-    public override void OnInspectorGUI()
-    {
-        MapGenerator mapGen = (MapGenerator)target;
+	public override void OnInspectorGUI() {
+		MapGenerator mapGen = (MapGenerator)target;
 
-        
-        if (GUILayout.Button("Generate"))
-        {
-            mapGen.GenerateMap();
-        }
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
 
-        DrawDefaultInspector();
-    }
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor ();
+		}
+	}
 }
-
